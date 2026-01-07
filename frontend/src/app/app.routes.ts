0ed6b22a7285afc3e9a0907620ed397810869ee0
jesuuslopeez <jlopper299@g.educaand.es>
2026-01-07 12:48:27 +0100

    FASE4 - DIW

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { StyleGuide } from './pages/style-guide/style-guide';
+import { Competitions } from './pages/competitions/competitions';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'style-guide', component: StyleGuide },
+  { path: 'competiciones', component: Competitions },
 ];
