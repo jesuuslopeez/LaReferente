0fb4dcd64eee0c659ca258ff1fc8e03c69e9f67b
jesuuslopeez <jlopper299@g.educaand.es>
2026-02-10 09:47:36 +0100

    Preparación de archivos

--- frontend/src/app/pages/city/city.ts
+++ frontend/src/app/pages/city/city.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlayerCard } from '../../components/shared/player-card/player-card'
 
 @Component({
     selector: 'app-city',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, PlayerCard],
     templateUrl: './city.html',
     styleUrl: './city.scss',
 })
